dcd464eb6fe8cbfbd45ab4ff2857f2e70bb544cc
leebj <redgpcmffld@gmail.com>
2021-05-19 13:37:37 +0900

    Add: locations view, serializer 추가

--- projects/serializers.py
+++ projects/serializers.py
@@ -10,22 +10,25 @@ class LocationSerializer(serializers.ModelSerializer):
 
 class LocationViewSerializer(serializers.ModelSerializer):
     site = serializers.SerializerMethodField()
-    type = serializers.CharField(max_length=3)
     range = serializers.CharField(max_length=10)
 
     class Meta:
         model = Location
         fields = ['site', 'type', 'name', 'address', 'latitude', 'longitude', 'range', 'is_allow']
 
-    def get_site_name(self, obj):
+    def get_site(self, obj):
         return obj.site.name
 
-    def get_type(self, attrs):
-        if attrs == 1:
-            attrs = '상차지'
+    def get_type(self, obj):
+        print(obj)
+        if obj.type == True:
+            obj.type = '상차지'
         else:
-            attrs = '하차지'
-        return attrs
+            obj.type = '하차지'
+        return obj
 
     def get_range(self, attrs):
         return f'{attrs}m'
+
+    def get_attribute(self, instance):
+        print(instance)
