e4bb1d3692d35ddc61ab832ecf5a739ac396b170
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-05-21 11:05:02 +0900

    Refactor: Site Convention 수정

--- projects/serializers.py
+++ projects/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 
 from .models import Site, Location, Resource
 
+
 class SiteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Site
--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SiteView
 
 urlpatterns = [
-    path('/sites', SiteView.as_view()),
     path('/sites/<int:site_id>', SiteView.as_view()),
+    path('/sites', SiteView.as_view())
 ]
