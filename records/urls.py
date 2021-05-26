144db592bbbadb57f06d23b2827f410425f30fd2
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-05-26 13:54:13 +0900

    Add: 현장 작업량 그래프

--- records/urls.py
+++ records/urls.py
@@ -1,7 +1,8 @@
 from django.urls    import path
-from .views import DriveStartView, DriveRecordView, DriveEndView, ProgressView
+from .views import DriveStartView, DriveRecordView, DriveEndView, ProgressView, GraphView
 
 urlpatterns = [
+    path('/graph', GraphView.as_view()),
     path('/<int:drive_record_id>', DriveRecordView.as_view(), name='read_drive_record_detail'),
     path('', DriveRecordView.as_view(), name='read_drive_record_list'),
     path('/start', DriveStartView.as_view(), name='create_drive_record'),
