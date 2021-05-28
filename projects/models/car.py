cdcb004918af1350aac28812ff115cdd816a56e6
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-05-28 12:19:02 +0900

    Modify: 차량 serializer 수정

--- projects/models/car.py
+++ projects/models/car.py
@@ -48,5 +48,9 @@ class CarViewSerializer(serializers.ModelSerializer):
         return {'site_id': obj.site.pk, 'name': obj.site.name}
 
     def get_driver(self, obj):
-        return {'driver_id': obj.driver.get().pk, 'phone_number': obj.driver.get().phone_number,
-                'name': obj.driver.get().name}
+        result = [{
+            'driver_id': driver.pk,
+            'name': driver.name,
+            'phone_number': driver.phone_number
+        } for driver in Driver.objects.filter(is_active=True)]
+        return result
