4628be83ab7caa89f0b21f03061a8add40b885fc
leebj <redgpcmffld@gmail.com>
2021-06-01 12:10:29 +0900

    modify: drive_record url 수정

--- records/urls/drive_records.py
+++ records/urls/drive_records.py
@@ -2,8 +2,8 @@ from django.urls import path
 from ..views.drive_records import DriveRecordView, DriveRecordDetailView, DriveRecordListExportView
 
 urlpatterns = [
-    path('/<int:drive_record_id>/details', DriveRecordDetailView.as_view(), name='create_drive_route|read_drive_routes'),
-    path('/details', DriveRecordDetailView.as_view(), name='read_all_drive_routes'),
+    path('/<int:drive_record_id>/routes', DriveRecordDetailView.as_view(), name='create_drive_route|read_drive_routes'),
+    path('/all/routes', DriveRecordDetailView.as_view(), name='read_all_drive_routes'),
     path('/<int:drive_record_id>', DriveRecordView.as_view(), name='read_drive_record_detail|update_drive_record'),
     path('', DriveRecordView.as_view(), name='read_drive_record_list|create_drive_record'),
     path('/export', DriveRecordListExportView.as_view())
